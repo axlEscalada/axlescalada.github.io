d3961abb18eeebd07bff6719405eb0af83acfc88
axl <axescalada@gmail.com>
2024-07-22 00:50:04 -0300

    finish style changes

---
+++ shell.nix
@@ -0,0 +1,8 @@
+with (import <nixpkgs> {}); let
+  unstable = import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/50eb7ecf4cd0a5756d7275c8ba36790e5bd53e33.tar.gz");
+in
+  pkgs.mkShell {
+    nativeBuildInputs = with pkgs.buildPackages; [
+      zola
+    ];
+  }
